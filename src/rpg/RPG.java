959561446482e6b29741377cb1ae16d9ef7bad60
SDKR <ecgk@live.dk>
2015-09-28 12:55:51 +0200

    john

--- src/rpg/RPG.java
+++ src/rpg/RPG.java
@@ -18,7 +18,7 @@ public class RPG {
      */
     
     // HEJSA!
-    //WEEEEEEEEEEE
+    //WEEEEEEEEEEEEEEEEEEEE
     public static void main(String[] args) {
 
         Builder build = new Builder(5);
